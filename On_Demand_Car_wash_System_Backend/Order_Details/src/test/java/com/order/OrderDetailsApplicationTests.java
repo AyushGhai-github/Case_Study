8f4c6cfc1c289dc8b376059df9e213374629c9bc
AyushGhai-github <97190388+AyushGhai-github@users.noreply.github.com>
2022-04-29 13:19:46 +0530

    Update OrderDetailsApplicationTests.java

--- On_Demand_Car_wash_System_Backend/Order_Details/src/test/java/com/order/OrderDetailsApplicationTests.java
+++ On_Demand_Car_wash_System_Backend/Order_Details/src/test/java/com/order/OrderDetailsApplicationTests.java
@@ -1,13 +1,45 @@
 package com.order;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
-class OrderDetailsApplicationTests {
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.customerdetails.model.Customers;
+import com.customerdetails.services.CustomerServiceImpl;
+
+
+
+class CustomerServiceImplTest {
+
+	CustomerServiceImpl customerServiceImplMock = mock(CustomerServiceImpl.class);
 
 	@Test
-	void contextLoads() {
+	@DisplayName("Find customer by Name")
+	void findByName() {
+		Customers customer = new Customers(1, "ayush", "pass","ayushghai65@gmail.com","Ayush", "Address",98709,false, "true", "CUSTOMER");
+		when(customerServiceImplMock.findByCustomerName("customer")).thenReturn(customer);
+			
+	}
+	
+	@Test
+	@DisplayName("Add customer")
+	void addCustomer() {
+		Customers customer = new Customers(1, "ayush", "pass","ayushghai65@gmail.com","Ayush", "Address",98709,false, "true", "CUSTOMER");
+		when(customerServiceImplMock.addCustomer(customer)).thenReturn(customer);
+				
+		assertEquals(customer, customerServiceImplMock.addCustomer(customer));
+	
 	}
 
+	@Test
+	void updateProfile() {
+		Customers customer = new Customers(1, "ayush", "pass","ayushghai65@gmail.com","Ayush", "Address",98709,false, "true", "CUSTOMER");
+		when(customerServiceImplMock.updateCustomerById(customer)).thenReturn(customer);
+		assertEquals(customer, customerServiceImplMock.updateCustomerById(customer));
+		
+	}
 }
