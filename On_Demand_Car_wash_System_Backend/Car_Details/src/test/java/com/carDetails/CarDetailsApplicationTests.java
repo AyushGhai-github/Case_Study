ad2dc09ea33dec13279293ffbbfb2baebb317465
AyushGhai-github <97190388+AyushGhai-github@users.noreply.github.com>
2022-04-29 12:46:59 +0530

    Update CarDetailsApplicationTests.java

--- On_Demand_Car_wash_System_Backend/Car_Details/src/test/java/com/carDetails/CarDetailsApplicationTests.java
+++ On_Demand_Car_wash_System_Backend/Car_Details/src/test/java/com/carDetails/CarDetailsApplicationTests.java
@@ -1,13 +1,46 @@
 package com.carDetails;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CarDetailsApplicationTests {
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.cardetails.model.Customers;
+import com.cardetails.services.CustomerServiceImpl;
+
+
+
+class CustomerServiceImplTest {
+
+	CustomerServiceImpl customerServiceImplMock = mock(CustomerServiceImpl.class);
 
 	@Test
-	void contextLoads() {
+	@DisplayName("Find customer by Name")
+	void findByName() {
+		Customers customer = new Customers(1, "ayush", "pass","ayushghai65@gmail.com","Ayush", "Address",98709,false, "true", "CUSTOMER");
+		when(customerServiceImplMock.findByCustomerName("customer")).thenReturn(customer);
+			
+	}
+	
+	@Test
+	@DisplayName("Add customer")
+	void addCustomer() {
+		Customers customer = new Customers(1, "ayush", "pass","ayushghai65@gmail.com","Ayush", "Address",98709,false, "true", "CUSTOMER");
+		when(customerServiceImplMock.addCustomer(customer)).thenReturn(customer);
+				
+		assertEquals(customer, customerServiceImplMock.addCustomer(customer));
+	
 	}
 
+	@Test
+	void updateProfile() {
+		Customers customer = new Customers(1, "ayush", "pass","ayushghai65@gmail.com","Ayush", "Address",98709,false, "true", "CUSTOMER");
+		when(customerServiceImplMock.updateCustomerById(customer)).thenReturn(customer);
+		assertEquals(customer, customerServiceImplMock.updateCustomerById(customer));
+		
+	}
 }
